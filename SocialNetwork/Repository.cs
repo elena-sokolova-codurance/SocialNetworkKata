e61f8c7906d279116b5e1b184f1ab5ca636e468e
Paco Benitez <paco.benitez@sfd.gov.sa>
2023-02-08 15:25:17 +0100

    multimap??

--- SocialNetwork/Repository.cs
+++ SocialNetwork/Repository.cs
@@ -2,14 +2,21 @@ namespace SocialNetwork;
 
 public class Repository
 {
-    
+    private Dictionary<string, List<string>> _storage = new();
+
     public virtual void SaveMessage(string userName, string message)
     {
-        throw new NotImplementedException();
+        var currentMessages = _storage[userName];
+        if (currentMessages == null)
+        {
+            currentMessages = new List<string>();
+        }
+        currentMessages.Add(message);
+        _storage[userName] = currentMessages;
     }
 
     public List<string> ReadMessagesFromUser(string userName)
     {
-        return null;
+        return _storage[userName];
     }
 }
