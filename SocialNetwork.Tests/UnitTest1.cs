af9dfdb5d5d9a96d06711fef6f27b5aa6c5f0662
sokolovaeldm <sokolova.el.dm@gmail.com>
2023-02-08 11:15:25 +0100

    test

--- SocialNetwork.Tests/UnitTest1.cs
+++ SocialNetwork.Tests/UnitTest1.cs
@@ -1,9 +1,21 @@
 namespace SocialNetwork.Tests;
 
-public class UnitTest1
+public class SocialNetworkTest
 {
     [Fact]
-    public void Test1()
+    public void ShouldPostMessage()
     {
+        //Given: Alice
+        
+        // When she send "I love the weather today"
+        
+        // And Bob ask for Alice's message
+        
+        // Then Bob can read Alice message "I love the weather today"
+        //var expectedResult = "I love the weather today";
+        
+        
+        
+        //Assert.Equal(expectedResult, )
     }
 }
